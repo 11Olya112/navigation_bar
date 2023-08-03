930bcfd5c34b476df9f7ea7bc69174d9c1a63b23
11Olya112 <OlyaBushovska767@gmail.com>
2023-08-03 13:27:45 +0300

    solution

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import {
-  useJsApiLoader, GoogleMap, Marker, Autocomplete,
+  GoogleMap, Marker, LoadScript,
 } from '@react-google-maps/api';
+import AutocompleteWrapper from './AutocompleteWrapper';
 import home from '../assets/HomeIndicator.svg';
 import top from '../assets/StatusBar.svg';
 import './NavBar.scss';
@@ -11,42 +12,29 @@ export const NavBar: React.FC = () => {
 
   const apiKey = 'AIzaSyAYd6GORf3u4BNL9OSI4QaYaRGQVLsOYfU';
 
-  const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: apiKey,
-    libraries: ['places'], // Додали 'places' до бібліотек
-  });
-
-  if (!isLoaded) {
-    // eslint-disable-next-line no-console
-    console.log('Error');
-  }
-
   const center = { lat: 48.8584, lng: 2.2945 };
 
   return (
-    <div className="starter">
-      <img src={top} alt="StatusBar" />
-      <button type="button" onClick={() => map?.panTo(center)}>Home</button>
-      {' '}
-      {/* Змінили maps на map */}
-      <Autocomplete>
-        <input type="text" placeholder="Start" />
-      </Autocomplete>
-      <Autocomplete>
-        <input type="text" placeholder="End" />
-      </Autocomplete>
-      <GoogleMap
-        center={center}
-        zoom={15}
-        mapContainerStyle={{ width: '100%', height: '100%' }}
-        onLoad={maps => setMap(maps)}
-      >
-        <Marker position={center} />
-        {}
-      </GoogleMap>
-      <div className="img-container">
-        <img src={home} alt="Home" />
+    <LoadScript googleMapsApiKey={apiKey} libraries={['places']} loadingElement={<div>Loading...</div>}>
+      <div className="starter">
+        <img src={top} alt="StatusBar" />
+        <button type="button" onClick={() => map?.panTo(center)}>Home</button>
+        <AutocompleteWrapper placeholder="Start" />
+        <AutocompleteWrapper placeholder="End" />
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          onLoad={maps => setMap(maps)}
+        >
+          <Marker position={center} />
+          {}
+        </GoogleMap>
+        <div className="img-container">
+          <img src={home} alt="Home" />
+        </div>
       </div>
-    </div>
+
+    </LoadScript>
   );
 };
