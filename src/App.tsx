5fe658c498810dc2752204afdf8bb34ee1ba8887
11Olya112 <OlyaBushovska767@gmail.com>
2023-08-11 01:59:31 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  GoogleMap, Marker, LoadScript, DirectionsRenderer,
+} from '@react-google-maps/api';
+import NavBar from './components/NavBar';
+import home from './assets/HomeIndicator.svg';
 import './App.scss';
-import { NavBar } from './components/NavBar';
 
 export const App: React.FC = () => {
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+
+  const [directionsResponse, setDirectionsResponse]
+  = useState<google.maps.DirectionsResult | null>(null);
+
+  const apiKey = 'AIzaSyAYd6GORf3u4BNL9OSI4QaYaRGQVLsOYfU';
+
+  const center = { lat: 48.8584, lng: 2.2945 };
+
   return (
-    <div className="starter">
-      <NavBar />
-    </div>
+    <LoadScript googleMapsApiKey={apiKey} libraries={['places']} loadingElement={<div>Loading...</div>}>
+      <div className="map-container">
+        <NavBar
+          map={map}
+          setDirectionsResponse={setDirectionsResponse}
+        />
+
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          onLoad={maps => setMap(maps)}
+        >
+          <Marker position={center} />
+          {directionsResponse && <DirectionsRenderer directions={directionsResponse} />}
+        </GoogleMap>
+
+        <div className="img-container">
+          <img src={home} alt="Home" />
+        </div>
+        {' '}
+
+      </div>
+    </LoadScript>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,40 +1,132 @@
-import React, { useState } from 'react';
-import {
-  GoogleMap, Marker, LoadScript,
-} from '@react-google-maps/api';
-import AutocompleteWrapper from './AutocompleteWrapper';
-import home from '../assets/HomeIndicator.svg';
+import React, { useRef, useState } from 'react';
+import { Autocomplete } from '@react-google-maps/api';
 import top from '../assets/StatusBar.svg';
 import './NavBar.scss';
 
-export const NavBar: React.FC = () => {
-  const [map, setMap] = useState<google.maps.Map | null>(null);
+interface NavBarProps {
+  map: google.maps.Map | null;
+  setDirectionsResponse: any;
+}
 
-  const apiKey = 'AIzaSyAYd6GORf3u4BNL9OSI4QaYaRGQVLsOYfU';
-
+const NavBar: React.FC<NavBarProps> = ({ map, setDirectionsResponse }) => {
   const center = { lat: 48.8584, lng: 2.2945 };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [distance, setDistance] = useState<string>('');
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [duration, setDuration] = useState<string>('');
+
+  const destiantionRef = useRef<HTMLInputElement>(null);
+  const originRef = useRef<HTMLInputElement>(null);
+
+  const calculateRoute = async () => {
+    if (!originRef.current?.value || !destiantionRef.current?.value) {
+      return;
+    }
+
+    const directionsService = new google.maps.DirectionsService();
+    const request: google.maps.DirectionsRequest = {
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+
+    try {
+      const results = await directionsService.route(request);
+
+      if (results) {
+        setDirectionsResponse(results);
+        setDistance(results.routes[0]?.legs[0]?.distance?.text || '');
+        setDuration(results.routes[0]?.legs[0]?.duration?.text || '');
+      }
+    } catch (error) {
+      // Handle the error
+    }
+  };
+
+  const clearRoute = () => {
+    setDirectionsResponse(null);
+    setDistance('');
+    setDuration('');
+    if (originRef.current) {
+      originRef.current.value = '';
+    }
+
+    if (destiantionRef.current) {
+      destiantionRef.current.value = '';
+    }
+  };
+
   return (
-    <LoadScript googleMapsApiKey={apiKey} libraries={['places']} loadingElement={<div>Loading...</div>}>
-      <div className="starter">
-        <img src={top} alt="StatusBar" />
-        <button type="button" onClick={() => map?.panTo(center)}>Home</button>
-        <AutocompleteWrapper placeholder="Start" />
-        <AutocompleteWrapper placeholder="End" />
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={{ width: '100%', height: '100%' }}
-          onLoad={maps => setMap(maps)}
-        >
-          <Marker position={center} />
-          {}
-        </GoogleMap>
-        <div className="img-container">
-          <img src={home} alt="Home" />
+    <div className="navbar">
+      <img src={top} alt="StatusBar" />
+      <div className="container">
+        <div className="controls">
+          <button
+            type="button"
+            className="button button--home"
+            onClick={() => map?.panTo(center)}
+          >
+            Home
+          </button>
+          <button
+            type="button"
+            className="button button--calculate"
+            onClick={calculateRoute}
+          >
+            Calculate
+          </button>
+          <button
+            type="button"
+            className="button button--clear"
+            onClick={clearRoute}
+          >
+            Clear
+          </button>
+        </div>
+        <div className="input-group">
+          <div className="autocomplete">
+            <Autocomplete>
+              <input
+                className="input input--start"
+                type="text"
+                placeholder="Start"
+                ref={originRef}
+              />
+            </Autocomplete>
+          </div>
+          <div className="autocomplete">
+            <Autocomplete>
+              <input
+                className="input input--end"
+                type="text"
+                placeholder="End"
+                ref={destiantionRef}
+              />
+            </Autocomplete>
+          </div>
+        </div>
+        <div className="results">
+          <div className="small-input">
+            <span className="input-label">Distance:</span>
+            <input
+              className="input input--distance"
+              type="text"
+              value={distance}
+            />
+          </div>
+          <div className="small-input">
+            <span className="input-label">Duration:</span>
+            <input
+              className="input input--duration"
+              type="text"
+              value={duration}
+            />
+          </div>
         </div>
       </div>
-
-    </LoadScript>
+    </div>
   );
 };
+
+export default NavBar;
